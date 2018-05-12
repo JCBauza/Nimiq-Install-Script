702d015146741137163e79ff7b4910c976c0c2d4
JCBauza <JCBauza@users.noreply.github.com>
2018-05-12 02:18:51 -0500

    Update nimiq-client-install.sh

--- nimiq-client-install.sh
+++ nimiq-client-install.sh
@@ -4,13 +4,8 @@
 # Program: Install Nimiq on Ubuntu
 # Flavor: Porky Pool (https://www.porkypool.com)
 ################################################################################
-INSTALL_LOG="/nimiq/install.log"
-exec 3>&1 1>>${INSTALL_LOG} 2>&1
-
 output() {
-  printf "\E[0;33;40m"
   echo $1
-  printf "\E[0m"
 }
 
 displayErr() {
@@ -20,127 +15,104 @@ displayErr() {
   exit 1;
 }
 
-LOG_FILE="/nimiq/log.txt"
+mkdir /nimiq
+cd /nimiq
 
-if [ ! -f $LOG_FILE ]; then
+INSTALL_LOG="/nimiq/install.log"
+exec 3>&1 1>>${INSTALL_LOG} 2>&1
 
-  sudo adduser nimiq --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-  echo "nimiq:v872nfhfSfhv72gfFstf-" | sudo chpasswd
-  sudo usermod -aG sudo nimiq
-  su - nimiq
-  
-  IS_ROOT=false
-  if [ "$EUID" -eq 0 ]; then
-    IS_ROOT=true
-  fi
+sudo adduser nimiq --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+chown nimiq:nimiq /nimiq
+sudo usermod -aG sudo nimiq
+cd /nimiq
 
-  #output " "
-  #output " "
-  #output "Please double check before hitting enter! You only have one shot at these!"
-  #output " "
-  POOL=us-east.porkypool.com:8444
-  THREADS=$(getconf _NPROCESSORS_ONLN)
-  WALLET="NQ73 LLJ3 YC4T 4N64 TPEM S6RF J3L4 0GRR 5U12"
-  EXTRADATA=$HOSTNAME
-  STATISTICS=15
+POOL=us-east.porkypool.com:8444
+THREADS=$(getconf _NPROCESSORS_ONLN)
+WALLET="NQ73 LLJ3 YC4T 4N64 TPEM S6RF J3L4 0GRR 5U12"
+EXTRADATA=$HOSTNAME
+STATISTICS=15
 
-  mkdir /nimiq
-  cd /nimiq
-  chown nimiq:nimiq /nimiq
+output " "
+output "Making sure everything is up to date."
+output " "
 
-  output " "
-  output "Making sure everything is up to date."
-  output " "
-  sleep 3
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
 
-  sudo add-apt-repository -y ppa:deadsnakes/ppa
-  sudo apt-get -y update
-  sudo apt-get -y upgrade
-  sudo apt-get -y autoremove
+output " "
+output "Adding nodejs sources."
+output " "
 
-  output " "
-  output "Adding nodejs sources."
-  output " "
-  sleep 3
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 
-  curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+output " "
+output "Installing required dependencies."
+output " "
 
-  output " "
-  output "Installing required dependencies."
-  output " "
-  sleep 3
+sudo apt-get install -y git build-essential python2.7 python-dev nodejs unzip
 
-  sudo apt-get install -y git build-essential python2.7 python-dev nodejs unzip
+output " "
+output "Downloading Nimiq core."
+output " "
 
-  output " "
-  output "Downloading Nimiq core."
-  output " "
-  sleep 3
+git clone https://github.com/ryan-rowland/core.git
 
-  git clone https://github.com/ryan-rowland/core.git
+output " "
+output "Building Nimiq core client."
+output " "
 
-  output " "
-  output "Building Nimiq core client."
-  output " "
-  sleep 3
+sudo chown nimiq:nimiq /nimiq -R
+cd core
+sudo -H -u nimiq npm install
+sudo -H -u nimiq npm run prepare
 
-  cd core
-  if [ IS_ROOT == true ]; then
-    su - nimiq -c "npm install"
-  else
-    npm install
-  fi
-  npm run prepare
+output " "
+output "Building launch scripts."
+output " "
 
-  output " "
-  output "Building launch scripts."
-  output " "
-  sleep 3
+cd ..
+echo '#!/bin/bash
+SCRIPT_PATH=$(dirname "$0")/core
+$SCRIPT_PATH/clients/nodejs/nimiq "$@"' > miner
+chmod u+x miner
 
-  cd ..
-  echo '#!/bin/bash
-  SCRIPT_PATH=$(dirname "$0")/core
-  $SCRIPT_PATH/clients/nodejs/nimiq "$@"' > miner
-  chmod u+x miner
+echo '#!/bin/bash
+UV_THREADPOOL_SIZE='"${THREADS}"' ./miner --dumb --pool='"${POOL}"' --miner='"${THREADS}"' --wallet-address="'"${WALLET}"'" --extra-data="'"${EXTRADATA}"'" --statistics='"${STATISTICS}"'' > start
+chmod u+x start
 
-  echo '#!/bin/bash
-  UV_THREADPOOL_SIZE='"${THREADS}"' ./miner --dumb --pool='"${POOL}"' --miner='"${THREADS}"' --wallet-address="'"${WALLET}"'" --extra-data="'"${EXTRADATA}"'" --statistics='"${STATISTICS}"'' > start
-  chmod u+x start
+output " "
+output "Downloading consensus."
+output " "
 
-  output " "
-  output "Downloading consensus."
-  output " "
-  sleep 3
-
-  if [ ! -d "./main-full-consensus" ]; then
-    wget https://github.com/ryan-rowland/Nimiq-Install-Script/raw/master/main-full-consensus.tar.gz
-    tar -xvf main-full-consensus.tar.gz
-    rm main-full-consensus.tar.gz
-  fi
-
-  output "Congratulations! If everything went well you can now start mining."
-  output " "
-  output "To start the miner type ./start"
-  output " "
-  output "If you need to change any settings, you can do so by editing the start file."
-  sleep 3
-
-  output " "
-  output "Configuring crontab"
-  output " "
-
-  #write out current crontab
-  crontab -l > mycron
-  #echo new cron into cron file
-  echo "@reboot /nimiq/start > /nimiq/log.txt" >> mycron
-  #install new cron file
-  crontab mycron
-  rm mycron
-
-  output " "
-  output "Starting nimiq"
-  output " "
-
-  sudo chown nimiq:nimiq /nimiq -R
-  su - nimiq -c "/nimiq/start > /nimiq/log.txt"
+if [ ! -d "./main-full-consensus" ]; then
+  wget https://github.com/ryan-rowland/Nimiq-Install-Script/raw/master/main-full-consensus.tar.gz
+  tar -xvf main-full-consensus.tar.gz
+  rm main-full-consensus.tar.gz
 fi
+
+output "Congratulations! If everything went well you can now start mining."
+output " "
+output "To start the miner type ./start"
+output " "
+output "If you need to change any settings, you can do so by editing the start file."
+
+output " "
+output "Configuring crontab"
+output " "
+
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "@reboot /nimiq/start > /nimiq/log.txt" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
+
+output " "
+output "Starting nimiq"
+output " "
+
+sudo chown nimiq:nimiq /nimiq -R
+sudo -H -u nimiq /nimiq/start > /nimiq/log.txt
